46fb9fdd48266325180c06e813f48446776b5b74
Rhyst223 <31656440+Rhyst223@users.noreply.github.com>
2020-06-12 12:34:44 +0100

    Updating BCs on left/right, custom BC values

--- RadialDiffusionModel/ensemble_functions.py
+++ RadialDiffusionModel/ensemble_functions.py
@@ -41,15 +41,26 @@ def Diff_Coeff(L,Kp, full=False):
     
 def Crank_Nicolson(dt,nt,dL,L,f,Dlist,Q,lbc=None,rbc=None,ltype='d',rtype='d'):
     '''
-    Must give D for each time in a list of lists size (nt)
-    To do: edit to handle Dirichlet right and Neumann left BC 
+    Code for running the Modified Crank Nicolson numerical scheme for radial diffusion as in (Welling et al, 2012).
+    Currently functionality requires Diffusion array to extend for one timestep beyond the final time
+    (TO DO: Input temporal boundary condition at final timestep)
+    
+    A source term Q can be provided with the same shape as D. 
+    
+    TO DO: Input functionality for loss term.
+    
+    If no BC's are given we assume Dirichlet with constant left and right boundary.
+    
+    Boundary conditions allowed on left/right are Dirichlet ('d') and Neumann ('n'). Custom BC values for each timestep
+    are allowed. Neumann BCs are calculated at order (dL)^2 (might put in functionality for lower order)
+    
     '''
     T = f.copy()
     
     if lbc==None:
-        lbc = T[0]
+        lbc = [T[0]]*nt
     if rbc == None:
-        rbc = T[-1]
+        rbc = [T[-1]]nt
         
     s = (0.5*dt/dL**2)    
     
@@ -60,39 +71,65 @@ def Crank_Nicolson(dt,nt,dL,L,f,Dlist,Q,lbc=None,rbc=None,ltype='d',rtype='d'):
         Dplus = Dlist[n+1]
         
         
-        Dl = np.array([L[i]**2 * 0.5*((D[i] + D[i-1])/2 + (Dplus[i] + Dplus[i-1])/2)/(L[i]-0.5*dL)**2 \
-                       if i!=int(len(L)-1) else 2 * L[i]**2 * 0.5*((D[i] + D[i-1])/2 + (Dplus[i] + Dplus[i-1])/2)/(L[i]-0.5*dL)**2 \
-                       for i in range(1,len(L))])
-        Dr = np.array([L[i]**2 * 0.5*((D[i] + D[i+1])/2 + (Dplus[i] + Dplus[i+1])/2)/(L[i]+0.5*dL)**2 \
-                       if i!=int(len(L)-1) else 0 for i in range(1,len(L))])
-        Dc = np.array([x+y for x,y in zip(Dl,Dr)])
+        #Dl = np.array([L[i]**2 * 0.5*((D[i] + D[i-1])/2 + (Dplus[i] + Dplus[i-1])/2)/(L[i]-0.5*dL)**2 \
+         #              if i!=int(len(L)-1) else 2 * L[i]**2 * 0.5*((D[i] + D[i-1])/2 + (Dplus[i] + Dplus[i-1])/2)/(L[i]-0.5*dL)**2 \
+          #             for i in range(1,len(L))])
+        #Dr = np.array([L[i]**2 * 0.5*((D[i] + D[i+1])/2 + (Dplus[i] + Dplus[i+1])/2)/(L[i]+0.5*dL)**2 \
+         #              if i!=int(len(L)-1) else 0 for i in range(1,len(L))])
+            
         
-        A = diags([-s*Dl[1:], 1+s*Dc, -s*Dr], [-1, 0, 1], shape=(len(L)-1, len(L)-1)).toarray() 
-        B1 = diags([s*Dl[1:], 1-s*Dc, s*Dr], [-1, 0, 1], shape=(len(L)-1, len(L)-1)).toarray() 
+        #A = diags([-s*Dl[1:], 1+s*Dc, -s*Dr], [-1, 0, 1], shape=(len(L)-1, len(L)-1)).toarray() 
+        #B1 = diags([s*Dl[1:], 1-s*Dc, s*Dr], [-1, 0, 1], shape=(len(L)-1, len(L)-1)).toarray() 
+        
+        Dl = np.array([L[i]**2 * 0.5*((D[i] + D[i-1])/2 + (Dplus[i] + Dplus[i-1])/2)/(L[i]-0.5*dL)**2 \
+               for i in range(1,len(L)-1)])
+        Dr = np.array([L[i]**2 * 0.5*((D[i] + D[i+1])/2 + (Dplus[i] + Dplus[i+1])/2)/(L[i]+0.5*dL)**2 \
+               for i in range(1,len(L)-1)])
+
+        Dc = np.array([x+y for x,y in zip(Dl,Dr)])
+
+        A = diags([-s*Dl[1:], 1+s*Dc, -s*Dr], [-1, 0, 1], shape=(len(L)-2, len(L)-2)).toarray() 
+        B1 = diags([s*Dl, 1-s*Dc, s*Dr], [0, 1, 2], shape=(len(L)-2, len(L))).toarray()
         
            
         #Input Dirichlet boundary for first row
         b = np.zeros(len(L)-1)
         if ltype=='d':
-            b[0] = 2*s * 0.5*((D[1] + D[0])/2 + (Dplus[1] + Dplus[0])/2) \
-                       / (L[1]-0.5*dL)**2 \
-                       *L[1]**2 * lbc
+            b[0] = s*Dl[0] * lbc[n]
+            #b[0] = 2*s * 0.5*((D[1] + D[0])/2 + (Dplus[1] + Dplus[0])/2) \
+             #          / (L[1]-0.5*dL)**2 \
+              #         *L[1]**2 * lbc
+        elif ltype=='n':
+            b[0] = -2*dL*s*Dl[0]*lbc[n]/3
+            A[0][0] -= (4/3) * s*Dl[0]
+            A[0][1] += s*Dl[0]/3
         
-        if rtype == 'd':         
-            b[-1] = 2*s * 0.5*((D[-2] + D[-3])/2 + (Dplus[-2] + Dplus[-3])/2) \
-                       / (L[-2]+0.5*dL)**2 \
-                       *L[-2]**2 * rbc            
+        if rtype == 'd':     
+            b[-1] = s*Dr[-1] * rbc[n] 
+            #b[-1] = 2*s * 0.5*((D[-2] + D[-3])/2 + (Dplus[-2] + Dplus[-3])/2) \
+             #          / (L[-2]+0.5*dL)**2 \
+              #         *L[-2]**2 * rbc     
+        elif rtype == 'n':
+            b[-1] = 2*dL*s*Dr[-1]*rbc[n]/3
+            A[-1,-1]-= s*Dr[-1]*4/3
+            A[-1,-2]+= s*Dr[-1]/3
             
         Qnew = (Q[n]+Q[n+1])/2
 
-        Tn = T.copy()
+        B = np.add(np.dot(B1,T[1:-1]),b+ dt*(Qnew[1:-1]))
+        T[1:-1] = np.linalg.solve(A,B)
+        
+        if ltype == 'd':
+            #Change this to drop the ros and column and implement Dirichlet
+            T[0] = lbc
+        elif rtype=='n':
+            T[0] = -2*dL*lbc/3 + 4*T[1]/3 - T[2]/3
+            
         if rtype == 'd':
             #Change this to drop the ros and column and implement Dirichlet
-            B = np.add(np.dot(B1,Tn[1:-1]),b+ dt*(Qnew[1:-1]))
-            T[1:-1] = np.linalg.solve(A,B)
-        else:
-            B = np.add(np.dot(B1,Tn[1:]),b+ dt*(Qnew[1:]))
-            T[1:] = np.linalg.solve(A,B)
+            T[-1] = rbc
+        elif rtype=='n':
+            T[-1] = 2*dL*rbc/3 + 4*T[-1]/3 - T[-2]/3
 
         res.append(T.copy())
 
