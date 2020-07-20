c5db59620432af039bc49afc5774f0eb95a6c20a
Rhyst223 <31656440+Rhyst223@users.noreply.github.com>
2020-07-20 09:57:18 +0100

    Update demo.py
    Created demo

--- RadialDiffusionModel/demo.py
+++ RadialDiffusionModel/demo.py
@@ -8,3 +8,15 @@ import itertools
 import scipy.stats as ss
 import argparse
 
+L = np.arange(2.5,6+dl,dl)
+f = PSD(L)    
+nt = int(172800/dt)
+
+#Ozeke 2014 DLL in SECONDS, for timesteps one more than you need
+D = [Diff_Coeff(L,3,full=True)[1]/86400]*int(nt+1)
+
+#Source term (for simple experiments I haven't used, but we might want to in the project!)
+Q = [np.zeros(len(L))] *int(nt+1)
+
+#Run diffusion model
+diffusion = Crank_Nicolson(dt,nt,dl,L,f,D,Q,lbc=None,rbc=[0]*nt,ltype='d',rtype='n')
